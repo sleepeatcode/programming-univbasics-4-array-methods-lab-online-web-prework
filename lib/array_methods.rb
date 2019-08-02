beae715f4b7ab1ff8a70e17c16bbcfe3c50be20a
Rachel T <rachmaybe@gmail.com>
2019-08-02 03:00:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,17 +9,18 @@ arrays.sort
 end
 
 def using_reverse(array)
-
+arrays = ["wow", "I", "am", "really", "learning", "arrays!"]
+arrays.reverse
 end
 
 def using_first(array)
-
+arrays.first
 end
 
 def using_last(array)
-
+arrays.last
 end
 
 def using_size(array)
-
+arrays.size
 end
